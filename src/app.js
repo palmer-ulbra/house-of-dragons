a93ef39c25fa245bdc0bac6adac9feeadaa0c865
Palmer Oliveira <palmer.oliveira@ulbra.br>
2026-06-23 18:09:32 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const tokenRouter = require('./routes/token')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
+  next()
+})
+
 app.use(express.json())
 app.use('/api', tokenRouter)
 app.use('/api', planRouter)
